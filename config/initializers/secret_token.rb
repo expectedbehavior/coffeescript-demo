ce6a74570ee355dcf35b59755fe8b4d35701c7da
Joel Meador <joel@expectedbehavior.com>
2011-02-07 02:34:49 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChatCaff::Application.config.secret_token = '5672ec665fcfb472482582b48e8a5470cbc68250f532b08037dc42cfccc35f5f99303fd232567b1fe899a44ec60671ef088d3fa58b65e8ad49cc6eecb395c8ad'
