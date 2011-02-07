c8dae2a5be9ea1f07227f76f70746721a4dd4f11
Joel Meador <joel@expectedbehavior.com>
2011-02-07 04:12:52 -0500

    make factory girl default instead of fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+# pull in our factories
+Dir["#{Rails.root}/test/factories/*.rb"].each {|file| require file }
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
